20e0df53834cbdd3dde21e6b7d677ddec5a03fa3
Gaston Corimayo <gastoncori@gmail.com>
2021-02-18 16:04:27 -0300

    terminado ver pais cap20

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+
+  termino: string='';
+  hayError:boolean=false;
+  paises: Country[]=[];
+
+  constructor(private paisService:PaisService) { }
 
   ngOnInit(): void {
   }
+
+  buscar(termino:string){
+    this.hayError=false;
+    this.termino=termino;
+ 
+  this.paisService.buscarCapital(this.termino)
+                   .subscribe((paises)=>{
+                          
+                           this.paises=paises;
+                     
+      },(err)=>{
+        this.hayError=true;
+        this.paises=[];
+     
+      }) ;
+ 
+  }
+ 
+  
 
 }
--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap,tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interfaces';
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-pais.component.css']
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+ pais!:Country;
+  constructor(private activatedRoute: ActivatedRoute,private paisService:PaisService) { }
 
   ngOnInit(): void {
+
+ this.activatedRoute.params
+ .pipe(
+   switchMap((param)=> this.paisService.getPaisPorAlpha(param.id)),
+   tap(console.log)
+ )
+ .subscribe(
+   pais=>{
+     
+     this.pais=pais;
+   }
+ );
+
+
+
+   // this.activatedRoute.params.subscribe(
+     // ({id})=>{
+       //        console.log(id);
+         //      this.paisService.getPaisPorAlpha(id).subscribe(
+          //       pais=>{
+            //       console.log(pais);
+              //   }
+              // );
+
+      //}
+   // )
   }
+
+
 
 }
